8c32adbd14d6bf71ebeb692a973ab83e5908ebfd
MihiraAthukorala <mahiathukorala2022@gmail.com>
2023-05-30 02:30:09 +0530

    done that

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ document.getElementById("start-timer").addEventListener("click", () => {
     }
     int = setInterval(displayTimer, 10);
 });
+
+function displayTimer() {
+    milliseconds += 1;
+    seconds = milliseconds == 100 ? (seconds + 1) % 60 : seconds;
+    minutes = seconds == 0 && milliseconds == 0 ? (minutes + 1) % 60 : minutes;
+    hours = minutes == 0 && seconds == 0 && milliseconds == 0 ? hours + 1 : hours;
+    milliseconds = milliseconds == 100 ? 0 : milliseconds;
+
+    let h = String(hours).padStart(2, "0");
+    let m = String(minutes).padStart(2, "0");
+    let s = String(seconds).padStart(2, "0");
+    let ms = String(milliseconds).padStart(2, "0");
+
+    timerRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+}
+
+document.getElementById("pause-timer").
+addEventListener("click", () => {
+    clearInterval(int);
+});
+
+document.getElementById("reset-timer").
+addEventListener("click", () => {
+    clearInterval(int);
+    [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+    timerRef.innerHTML = "00 : 00 : 00 : 00";
+});
