594a231b78a00be7656ee6dfb741ada48e765c87
MihiraAthukorala <mahiathukorala2022@gmail.com>
2023-05-29 23:28:25 +0530

    same

---
+++ script.js
@@ -0,0 +1,11 @@
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+
+let timerRef = document.querySelector(".timer-display");
+let int = null;
+
+document.getElementById("start-timer").addEventListener("click", () => {
+    if(int !== null) {
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer, 10);
+});
